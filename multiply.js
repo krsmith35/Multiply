a5e092c76ea51a17cfee56ef21f77e9e4fe62a35
Kelly <krsmith35@gmail.com>
2024-01-02 14:48:49 -0700

    Update multiply.js

--- multiply.js
+++ multiply.js
@@ -23,6 +23,8 @@ function setGame() {
             board[r][c] = Math.floor(Math.random()*11+1);
         }
     }
+    var tst = Math.floor(Math.random()*11+1);
+    console.log(tst);
     console.log(board[2][0]);
     
     for (let r = 0; r < rows; r++) {
